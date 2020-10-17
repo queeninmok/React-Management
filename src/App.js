595024ec3a6a21e1a05248176cc5aa3e04b6e026
queeninmok <imcho@insighton.kr>
2020-10-17 14:41:37 +0900

    Create

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <header className="App-header">
+        <img src={logo} lat="logo" />
+        <h2>Let's develop management system</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
